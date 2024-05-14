f2c7d69921442af81f638af8acea181f0ed464a4
xupan <2495526723@qq.com>
2024-05-14 17:14:14 +0800

    add

--- utilDemo/LoggerX/LoggerX.cpp
+++ utilDemo/LoggerX/LoggerX.cpp
@@ -1,40 +1,292 @@
 #include "LoggerX.h"
+#include <stdarg.h>
 
 using namespace moduleX;
 
 //init static var
 //TODO
-
-LoggerX::LoggerX()
-{
-
-}
-
-
-LoggerX::~LoggerX()
-{
-
-}
-
 LoggerX* moduleX::LoggerX::instance()
 {
 	static LoggerX instance;
+	instance._config.readConfig();
 	return &instance;
+}
+
+void moduleX::LoggerX::writeToTerminal(LogLevel level, const char *format, va_list args)
+{
+	string line = getLogCoutTime() + __NAME__(level) + getUserName() + getLogCoutThreadId() + BRACKETS_LEFT +
+		__FILE__ + SPACE + __FUNCTION__ + COLON + std::to_string(__LINE__) + BRACKETS_RIGHT;
+
+	char szBuff[MAX_BUFF_SIZE];
+	memset(szBuff, 0, sizeof(szBuff));
+	vsprintf(szBuff, format, args);
+
+	std::cout << line << szBuff << "\n";
+	fflush(stdout); 
+}
+
+void moduleX::LoggerX::writeToFile(LogLevel level, const char *format, va_list args)
+{
+	std::ostringstream oss; 
+	std::streambuf* pOldBuf = std::cout.rdbuf(oss.rdbuf());
+
+	char szBuff[MAX_BUFF_SIZE];
+	memset(szBuff, 0, sizeof(szBuff));
+	vsprintf(szBuff, format, args);
+
+	std::cout << szBuff;
+	std::string ret = oss.str();
+	std::cout.rdbuf(pOldBuf);
+
+	string line = getLogCoutTime() + __NAME__(level) + getUserName() + getLogCoutThreadId() + BRACKETS_LEFT +
+		__FILE__ + SPACE + __FUNCTION__ + COLON + std::to_string(__LINE__) + BRACKETS_RIGHT;
+	
+	string fullname = getFilefullname();
+
+	long fileSize = _filemgr.getFileSize(fullname);
+	if (fileSize > (long)atoi(_config["logMixSize"].data()) * MEGABYTES && _config["logBehavior "] == "1") {
+		string newFileName = getFilefullnameWithTime();
+		_filemgr.fileRename(fullname, newFileName);
+		_filemgr.createFile(fullname);
+	}
+	if (_config["logFileQueueSwitch"] == "off") {
+		mutex_file->lock();
+		ofstream file;
+		file.open(filePashAndName, ::ios::app | ios::out);
+		file << messages << message << line_effd;
+		file.close();
+		mutex_file->unlock();
+	}
+	else {
+		insertQueue(messages + message + line_effd, filePashAndName);
+	}
+	return 1;
+
+}
+
+std::string moduleX::LoggerX::getLogCoutTime(bool bBracket)
+{
+	time_t timep;
+	time(&timep);
+	char tmp[SIZE_64];
+	strftime(tmp, sizeof(tmp), "%Y-%m-%d %H:%M:%S", localtime(&timep));
+	if (!bBracket)
+	{
+		return tmp;
+	}
+	string tmp_str = tmp;
+	return BRACKETS_LEFT + tmp_str + BRACKETS_RIGHT;
+}
+
+
+
+
+std::string moduleX::LoggerX::getUserName()
+{
+#if defined __linux__   //linux system
+		uid_t userid;
+		struct passwd* pwd;
+		userid = getuid();
+		pwd = getpwuid(userid);
+		return pwd->pw_name;
+
+#elif defined _WIN32  //windows system
+		const int MAX_LEN = 100;
+		char szBuffer[MAX_LEN];
+		DWORD len = MAX_LEN;
+		if (GetUserName(szBuffer, &len))     //用户名保存在szBuffer中,len是用户名的长度
+			return szBuffer;
+#else  //outher system
+		return "";
+#endif
+}
+
+std::string moduleX::LoggerX::getLogCoutThreadId()
+{
+#ifdef __linux__
+	return to_string(syscall(__NR_gettid));
+#elif _WIN32
+	return to_string(GetCurrentThreadId());
+#endif
+}
+
+std::string moduleX::LoggerX::getFilefullname()
+{
+	return _config["logFilePath"]  + _config["logName"] + ".log";
+}
+
+std::string moduleX::LoggerX::getFilefullnameWithTime()
+{
+	return _config["logFilePath"] + _config["logName"] + getLogCoutTime(false) + ".log";
+}
+
+void LoggerX::writeLog(LogLevel level, const char *format, ...)
+{
+	//是否写日志
+	if (_config["logSwitch"] == "off")
+		return;
+
+	va_list args;
+	va_start(args, format);
+	if (_config["logFileSwitch"] == "on")
+	{
+		writeToTerminal(level, format, args);
+	}
+
+	if (_config["logTerminalSwitch"] == "on")
+	{
+		writeToFile(level, format, args);
+	}
+
+	va_end(args);
+
 }
 
 bool LoggerConfig::readConfig()
 {
 	_configMap.clear();
 
+	initConfig();
 
+	ifstream file;
+	file.open(_configfile);
 
+	if (!file.is_open())
+	{
+		cout << "readConfig error : " << _configfile << endl;
+		return false;
+	}
+
+	char str_c[SIZE_128] = { 0 };
+
+	string line;
+	while (getline(file, line)) {
+		if (!line.length()) {
+			continue;
+		}
+		string str_copy = line;
+		int j = 0;
+		for (int i = 0; i < line.length(); i++) {
+			if (line[i] == ' ')continue;
+			str_copy[j] = line[i];
+			j++;
+		}
+		str_copy.erase(j);
+		if (str_copy[0] == '#')  continue;
+
+		sscanf_s(str_copy.data(), "%[^=]", str_c);
+		auto iter = _configMap.find(str_c);
+		if (iter != _configMap.end()) {
+			sscanf_s(str_copy.data(), "%*[^=]=%s", str_c);
+			iter->second = str_c;
+		}
+		else {
+		}
+		
+	}
+	file.close();
 }
 
-bool moduleX::FileManage::createFile(std::string filename)
+void LoggerConfig::initConfig()
 {
-	ofstream file;
+	_configMap.clear();
 
-	file.open();
+	_configMap["logSwitch"] = "off";
+	_configMap["logFileSwitch"] = "off";
+	_configMap["logTerminalSwitch"] = "off";
+	_configMap["logFileQueueSwitch"] = "off";
+	_configMap["logName"] = "";
+	_configMap["logFilePath"] = "";
+	_configMap["logMixSize"] = "";
+	_configMap["logBehavior"]= "";
+	_configMap["logOutputLevelFile"] = "";
+	_configMap["logOutputLevelTerminal"] = "";
+	return ;
+}
 
+std::string LoggerConfig::operator[](string key) const
+{
+	auto iter = _configMap.find(key);
+	if (iter != _configMap.end())
+	{
+		return iter->second;
+	}
+}
 
+long FileManage::getCurrentTime()
+{
+	time_t t;
+	time(&t);
+
+	struct tm *p;
+	localtime_s(p, &t);
+
+	return mktime(p);
+}
+
+bool moduleX::FileManage::createFilePath(string path)
+{
+	int len = path.length();
+	if (!len) {
+		path = "log";
+#ifdef __linux__
+		if (0 != access(path.c_str(), F_OK)) {
+			if (-1 == mkdir(path.c_str(), 0)) {
+				return false;
+			}
+		}
+#elif _WIN32
+		if (0 != access(path.c_str(), 0)) {
+			if (-1 == mkdir(path.c_str())) {
+				return false;
+			}
+		}
+#endif
+	}
+	std::string fileName_cy(len, '\0');
+	for (int i = 0; i < len; i++) {
+		fileName_cy[i] = path[i];
+		if (fileName_cy[i] == '/' || fileName_cy[i] == '\\') {
+#ifdef __linux__
+			if (-1 == access(fileName_cy.c_str(), F_OK)) {
+				if (0 != mkdir(fileName_cy.c_str(), 0)) {
+					return false;
+				}
+			}
+#elif _WIN32
+			if (-1 == access(fileName_cy.c_str(), 0)) {
+				if (0 != mkdir(fileName_cy.c_str())) {
+					return false;
+				}
+			}
+#endif
+		}
+	}
+	return true;
+}
+
+long moduleX::FileManage::getFileSize(std::string filename)
+{
+	struct stat statbuf;
+	if (stat(filename.data(), &statbuf) == 0) {
+		return statbuf.st_size;
+	}
+	return -1;
+}
+
+bool moduleX::FileManage::isFileExist(string fileName)
+{
+#ifdef __linux__
+	return (access(fileName.data(), F_OK) != -1);
+#elif _WIN32
+	return (access(fileName.data(), 0) != -1);
+#endif
+}
+
+bool moduleX::FileManage::fileRename(string oldname, string newname)
+{
+	if (!rename(oldname.data(), newname.data())) {
+		cout << "File rename failed" << endl;
+	}
+	return 0;
 }
--- utilDemo/LoggerX/LoggerX.h
+++ utilDemo/LoggerX/LoggerX.h
@@ -2,16 +2,46 @@
 #define LOGGERX_H_
 #include <map>
 #include <string>
-
+#include <iostream>
 #include<fstream>
+#include <sstream>
+#include <ctime>
+#ifdef __linux__
+#include <unistd.h>
+#include <pwd.h>
+#endif
+
+#ifdef _WIN32
+#include<Windows.h>
+#endif
+
+#ifdef __linux__
+#include<unistd.h>
+#include<sys/syscall.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+#elif _WIN32
+#include<io.h>
+#include<direct.h>
+#endif
 
 
 
-
+#define __NAME__(name) #name
+#define MAX_BUFF_SIZE 512
+#define SIZE_64 64
+#define SIZE_128 128
 
 namespace moduleX {
 
 	using namespace std;
+	
+	const std::string  BRACKETS_LEFT = " [";
+	const std::string  BRACKETS_RIGHT = "] ";
+	const std::string SPACE = " ";
+	const std::string COLON = ":";
+	const std::string SLASH = "/";
+	const int MEGABYTES = 1048576;
 
 enum class LogLevel{
 	Error,
@@ -23,19 +53,31 @@ enum class LogLevel{
 
 class LoggerConfig{
 	typedef std::map<std::string, std::string> ConfigData;
+public:
+	void initConfig();
 
 	bool readConfig();
+
+	string operator[](string key) const;
 private:
-	ConfigData _configMap;
+	ConfigData		_configMap;
+	const string	_configfile = "./logconf.conf";
 };
 
 
 class FileManage{
 public:
+	bool createFilePath(string path);
 
-	bool createFile(std::string filename);
+	bool createFile(string filename);
 
 	long getCurrentTime();
+
+	long getFileSize(string filename);
+
+	bool isFileExist(string fileName);
+
+	bool fileRename(string oldname, string newname);
 };
 
 
@@ -51,17 +93,34 @@ public:
 public:
 	void writeLog(LogLevel level, const char *format, ...);
 
+	void writeToTerminal(LogLevel level, const char *format, va_list args);
+
+	void writeToFile(LogLevel level, const char *format, va_list args);
+
+	string getLogCoutTime(bool bBracket = true);
+
+	string getUserName();
+
+	string getLogCoutThreadId();
+
+	string getFilefullname();
+
+	string getFilefullnameWithTime();
+
+	LoggerConfig _config;
+
+	FileManage _filemgr;
 };
-
-
 
 
 
 }
 
 
-#define LOG_DEBUG(format, ...) do{moduleX::LoggerX::instance()->write_log(0, format, ##__VA_ARGS__); moduleX::Log::get_instance()->flush();}while(0)
-
-
+#define LOG_DEBUG(format, ...) do{moduleX::LoggerX::instance()->writeLog(LogLevel::Debug, format, ##__VA_ARGS__); moduleX::Log::instance()->flush();}while(0)
+#define LOG_INFO(format, ...) do{moduleX::LoggerX::instance()->writeLog(LogLevel::Info, format, ##__VA_ARGS__); moduleX::Log::instance()->flush();}while(0)
+#define LOG_WARN(format, ...) do{moduleX::LoggerX::instance()->writeLog(LogLevel::Warn, format, ##__VA_ARGS__); moduleX::Log::instance()->flush();}while(0)
+#define LOG_ERROR(format, ...) do{moduleX::LoggerX::instance()->writeLog(LogLevel::Error, format, ##__VA_ARGS__); moduleX::Log::instance()->flush();}while(0)
+#define LOG_TRACE(format, ...) do{moduleX::LoggerX::instance()->writeLog(LogLevel::Trace, format, ##__VA_ARGS__); moduleX::Log::instance()->flush();}while(0)
 
 #endif // LOGGERX_H_
