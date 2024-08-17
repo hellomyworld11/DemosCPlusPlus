10ebad4521eb73d6bbb3d3f120a616d994f17858
xupan <2495526723@qq.com>
2024-08-17 17:20:36 +0800

    add stl

---
+++ utilDemo/TinySTL/allocator.cpp
@@ -0,0 +1,12 @@
+#include "allocator.h"
+
+
+
+allocator::allocator()
+{
+}
+
+
+allocator::~allocator()
+{
+}
---
+++ utilDemo/TinySTL/testSTL.cpp
@@ -0,0 +1,12 @@
+#include "testSTL.h"
+
+
+int main(int argc, char *argv[])
+{
+	using namespace std;
+
+	cout << "hello world" << endl;
+
+	getchar();
+	return 0;
+}
